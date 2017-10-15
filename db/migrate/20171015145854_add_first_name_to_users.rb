f00b62602932d737b53e6edaf39c735586d4069c
JoeMugwaneza <muganezajoseph@gmail.com>
2017-10-15 17:13:21 +0200

    database architecture

---
+++ db/migrate/20171015145854_add_first_name_to_users.rb
@@ -0,0 +1,10 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :school, :string
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :enable, :boolean, default: false
+    add_column :users, :avatar, :string
+  end
+end
