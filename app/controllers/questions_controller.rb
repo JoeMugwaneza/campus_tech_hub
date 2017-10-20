d8543a54bd68d7c7cff5a6e36803b426f2891e10
JoeMugwaneza <muganezajoseph@gmail.com>
2017-10-20 14:37:46 +0200

    ability to add and edit a question and user can register. it is possible to log in and out now

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,48 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    find_question
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+
+    if @question.save
+
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def edit
+    find_question
+  end
+
+  def update
+    @question = Question.find_by(id: params[:id])
+    @question.update_attributes(question_params)
+
+    if @question.save
+      redirect_to @question
+    else 
+      render "edit"
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:subject_line, :description, :user_id)
+    
+  end
+
+  def find_question
     @question = Question.find_by(id: params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'questions#index'
   devise_for :users
 
+  
+  devise_scope :user do
+    get 'signout', to: 'devise/sessions#destroy', as: :signout
+  end
+
   resources :questions
 end
